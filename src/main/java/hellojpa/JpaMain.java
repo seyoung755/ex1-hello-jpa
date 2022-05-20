ffa5f6cc84e7a1fff18ac7bd9057112758864e25
seyeong <seyoung7555@naver.com>
2022-05-20 14:35:49 +0900

    feat: JPA 기본적인 동작 방식 구현

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,16 +2,38 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
-        EntityManager entityManager = emf.createEntityManager();
+        EntityManager em = emf.createEntityManager();
 
-        entityManager.close();
-        emf.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("HelloA");
+
+//            Member findMember = em.find(Member.class, 1L);
+            List<Member> members = em.createQuery("select m from Member as m", Member.class).getResultList();
+//            findMember.setName("helloJPA");
+            for (Member member : members) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
     }
 }
